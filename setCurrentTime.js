c37585fdb2ff49f8588daf127245e23852cfe570
SergeyShashin <s9997910@yandex.ru>
2025-11-11 14:37:42 +0300

    Life 111125 updated.

--- setCurrentTime.js
+++ setCurrentTime.js
@@ -1,6 +1,6 @@
 'use strict';
 
 window.onload = () => {
-  let interval = setInterval(() => timeEL.textContent = new Date(), 1000);
   let timeEL = document.querySelector('time');
+  let interval = setInterval(() => timeEL.textContent = new Date(), 1000);
 }
