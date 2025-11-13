479cc66dc52766493590571c0927018f1303327d
SergeyShashin <s9997910@yandex.ru>
2025-11-13 16:46:24 +0300

    Life 131125 updated.

--- classes/Human.js
+++ classes/Human.js
@@ -1,8 +1,72 @@
 class Human {
   anatomy = {
+    head: {
+      quantity: null,
+      brain: {
+        quantity: null,
+        medullaOblongata: {
+          substantiaGrisea: [] //серое вещество
+        }, //Продолговатый мозг
+        think() { }, //думать
+      },
+      eyes: {},
+      mouth: {},
+      nose: {},
+      nurse: {},
+    },
+    //центральная нервная система
+    centralNervousSystem: {
+
+      // нейроны
+      neurons: [
+        {
+          quantity: null,
+
+          cellBody: {}, // тело клетки
+          dendrites: [{}], // дендриты
+          axon: {}, // аксон
+
+          /**
+           * приём информации из вне
+           */
+          resivingInformationFromOutside() { },
+
+          /**
+           * обработка информации
+           */
+          dataProcessing() { },
+
+          /**
+           *хранение информации
+           */
+          dataStorage() { },
+
+          /**
+           * передача информации
+           */
+          transferOfInformation() { },
+
+          /**
+           * вывод информации вовне
+           */
+          outputOfInformationToOutside() {
+            let electricalSignal;
+            let chemicalSignal;
+          },
+
+          /**
+           *объединение нейронов 
+           */
+          connect() {
+
+          }
+        },
+      ]
+    }
 
   };
-  relatives;
+  relatives; // связи
+  siblings = []; //родственники 
   dateBirth;
   placeBirth;
   firstName;
@@ -10,6 +74,11 @@ class Human {
   patronymic;
   quantityDaysLife = 43800;
   numberDayLife = 1;
+  acquiredHabbits = []; //привычки          
+  experience = []; //опыт
+  skills = []; //навыки
+  beliefs = []; //убеждения
+  emotions = []; //эмоции
 
   constructor(dateBirth, placeBirth, firstName, surname, patronymic) {
     this.dateBirth = dateBirth;
@@ -62,5 +131,57 @@ class Human {
   getNumberDayLife() {
     return this.numberDayLife;
   }
+
+  setSiblings(siblings) {
+    this.siblings = siblings;
+  }
+
+  getSiblings() {
+    return this.siblings;
+  }
+
+  setAcquiredHabbits(acquiredHabbits) {
+    this.acquiredHabbits = acquiredHabbits;
+  }
+
+  getAcquiredHabbits() {
+    return this.acquiredHabbits;
+  }
+
+  setExperience(experience) {
+    this.experience = this.experience;
+  }
+
+  getExperience() {
+    return this.experience;
+  }
+
+  setSkills(skills) {
+    this.skills = skills;
+  }
+
+  getSkills() {
+    return this.skills;
+  }
+
+  choose() { } //выбирать
+
+  watch() { } //видеть
+
+  hear() { } //слышать
+
+  feel() { } //ощущать
+
+  remember() { } //запоминать
+
+  speak() { }
+
+  speakForTarget() { }
+
+  smileSimple() { }
+
+  smileForTarget() { }
+
+  go() { }
 
 }
