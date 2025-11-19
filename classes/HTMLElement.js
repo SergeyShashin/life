d1c90d8706c4cbd9e568c821e20355b48f520d14
SergeyShashin <s9997910@yandex.ru>
2025-11-19 12:42:42 +0300

    Life 191125 updated.

---
+++ classes/HTMLElement.js
@@ -0,0 +1,9 @@
+'use stict';
+
+class HTMLElement {
+  constructor(tagEl, idEl, classEl) {
+    this.idEl = idEl;
+    this.classEl = classEl;
+    this.tagEl = tagEl
+  }
+}
--- classes/Human.js
+++ classes/Human.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class Human {
   anatomy = {
     head: {
