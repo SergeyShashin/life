b6f062ae633d2453caec02167d0106a2d1a0d1f1
SergeyShashin <s9997910@yandex.ru>
2025-11-27 10:50:52 +0300

    Life 261125 updated.

--- classes/Settings.js
+++ classes/Settings.js
@@ -10,6 +10,24 @@ class Settings {
 
   createSettingsHTMLEl() {
     this.settingsEl = document.createElement('section');
+
+    let headerSettingsEl = document.createElement('h2');
+    headerSettingsEl.textContent = 'Настройки';
+
+    let sizeTeamEl = document.createElement('div');
+    sizeTeamEl.id = 'sizeTeam';
+    let headerSizeTeamEl = document.createElement('h4');
+    headerSettingsEl.textContent = 'размер группы';
+    let inputSizeTeamEl = document.createElement('input');
+    inputSizeTeamEl.id = 'inputSizeTeamEl';
+    inputSizeTeamEl.value = this.sizeTeam;
+
+    sizeTeamEl.appendChild(headerSizeTeamEl);
+    sizeTeamEl.appendChild(inputSizeTeamEl);
+
+    this.settingsEl.appendChild(headerSettingsEl);
+    this.settingsEl.appendChild(sizeTeamEl);
+
     return this.settingsEl
   }
 }
