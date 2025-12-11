eb36397d8d05ae37de04d50250f6519a9ba884d9
SergeyShashin <s9997910@yandex.ru>
2025-12-11 15:18:08 +0300

    Life 111225 updated.

--- classes/Capsule/Capsule.js
+++ classes/Capsule/Capsule.js
@@ -1,10 +1,15 @@
 'use strict';
 
 class Capsule {
+  //HTML элемент.
   capsuleHTMLEl;
+  //HTML элемент маршрутов.
   routesTableHTMLEl;
+  //Счётчик маршрутов.
   counterRoutes = 0;
-  routes = {};
+  //Маршруты.
+  routes = [];
+  //Варинты выбора точек отправления/назначения.
   coordinates = [
     'ул. Ленинского Комсомола, 21, Рязань, 54.62181095827267, 39.72299809466142',
     'ул. Островского, 111, Рязань, 54.617713826143124, 39.689331708034025',
@@ -21,51 +26,73 @@ class Capsule {
     'пл. Ленина, Рязань, 54.62906241524983, 39.73445184706703',
     'Центральный парк культуры и отдыха, 54.60871795574971, 39.725422482013734',
   ];
+  //Варианты выбора периодичности маршрута.
+  repeatVariants = ['ежедневно', 'еженедельно', 'ежемесячно', 'ежеквартально', 'ежегодно', 'cб+вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
 
+  //Конструктор базового класса.
   constructor() {
   }
 
+  /**
+  * Создаёт HTML элемент c заказом капсулы.
+  * @returns {HTMLElement} HTML элемент .
+  */
   createCapsuleHTMLEl() {
+    //Создание HTML элемента.
     this.capsuleHTMLEl = document.createElement('section');
+    //Добавление класса
     this.capsuleHTMLEl.classList.add('capsuleHTMLEl');
-
+    //Создание HTML элемента для заголовка.
     let headerCapsuleHTMLEl = document.createElement('h3');
+    //Установка текста заголовку.
     headerCapsuleHTMLEl.textContent = 'Капсула';
-
+    //Создание HTML элемента таблицы.
     this.routesTableHTMLEl = document.createElement('table');
 
+    //Создание HTML элемента строки таблицы.
     let trEl = document.createElement('tr');
 
     //HTML элемент. Точка назначения. Выбирается.
     let thDestinationEl = document.createElement('th');
+    //Установка текста точке назначения.
     thDestinationEl.textContent = 'назначение';
 
     //HTML элемент. Нужное время в точке назначения. Выбирается.
     let thTimeDestinationEl = document.createElement('th');
+    //Установка текста для времени в точке назначения.
     thTimeDestinationEl.textContent = 'нужное время в точке назначения';
 
     //HTML элемент. Точка назначения. Выбирается.
     let thStartingPointEl = document.createElement('th');
+    //Установка текста для времени в точки отправления.
     thStartingPointEl.textContent = 'отправление';
 
     //HTML элемент. Время в точке назначения. Устанавливается автоматически после ввода точки назначения и времени в точке назначения.
     let thTimeStartingPointEl = document.createElement('th');
+    //Установка текста для времени в точке отправления.
     thTimeStartingPointEl.textContent = 'время в точке отправления';
 
-    //HTML элемент. Размер капсулы. Выбирается. 1-12
+    //HTML элемент. Размер капсулы. Выбирается. Кол-во людей = от 1 до 12. Кол-во контейнеров, если доставляется груз от 1 до 12.
     let thSizeEl = document.createElement('th');
+    //Установка текста для размера капсулы.
     thSizeEl.textContent = 'размер';
 
     //HTML элемент. Размер капсулы. Выбирается. 1-12
     let thRepeatEl = document.createElement('th');
-    thRepeatEl.textContent = 'переодичность';
+    //Установка текста для периодичности.
+    thRepeatEl.textContent = 'периодичность';
 
-    //HTML элемент. Кнопка для добавления нового маршрута.
+    //HTML элемент. Ячейка для кнопка добавления нового маршрута.
     let thAddEl = document.createElement('th');
+    //HTML элемент. Кнопка для добавления нового маршрута.
     let btnEl = document.createElement('button');
+    //Устанавливаем класс кнопке.
     btnEl.classList.add('btn');
+    //Устанавливаем id кнопке.
     btnEl.id = 'btnAddRoute';
+    //Устанавливаем текст кнопке.
     btnEl.textContent = '+ маршрут';
+    //Добавляем кнопку в ячейку.
     thAddEl.appendChild(btnEl);
 
     trEl.appendChild(thDestinationEl);
@@ -82,18 +109,27 @@ class Capsule {
     this.capsuleHTMLEl.appendChild(this.routesTableHTMLEl);
 
     this.capsuleHTMLEl.appendChild(this.createDatalistCapsuleStartingPointAndDestinationHTMLEl());
+    this.capsuleHTMLEl.appendChild(this.createDatalistCapsuleRepeatVariantsHTMLEl());
 
     return this.capsuleHTMLEl;
   }
 
+  /**
+   * Добавление HTML элемента выбора маршрута.
+   */
   addRouteHTMLEl() {
+    //HTML элемент. Создаём строку таблицы.
     let trEl = document.createElement('tr');
+    //Добавляем id маршрута.
     trEl.dataset.idRoute = `route${this.counterRoutes}`;
+    //Количество колонок в таблице.
     let quantityColums = this.routesTableHTMLEl.querySelectorAll('th').length;
 
+    //Добавляем HTML элементы колонок.
     for (let col = 0; col < quantityColums; col++) {
       let tdEl = document.createElement('td');
 
+      //Если колонка последняя, добавляем кнопки для обновления/удаления маршрута.
       if (col === quantityColums - 1) {
         let buttons = new Control([
           { class: 'btn', id: 'btnApproveRoute', textRu: 'ага' },
@@ -101,6 +137,7 @@ class Capsule {
         ]);
         tdEl.appendChild(buttons.createControlHTMLEl());
       } else {
+        //Если колонка не последняя добавляем HTML элементы инпутов
         let inputForTdEl = document.createElement('input');
         tdEl.appendChild(inputForTdEl);
 
@@ -118,12 +155,14 @@ class Capsule {
           case 0:
           case 2:
             inputForTdEl.setAttribute('list', 'datalistCapsuleStartingPointAndDestinationHTMLEl');
-            inputForTdEl.type='search';
+            inputForTdEl.type = 'search';
+            break;
+          case 5:
+            inputForTdEl.setAttribute('list', 'datalistCapsuleRepeatVariantsHTMLEl');
+            inputForTdEl.type = 'search';
             break;
 
         }
-
-
       }
 
       trEl.appendChild(tdEl);
@@ -132,7 +171,6 @@ class Capsule {
 
     this.routesTableHTMLEl.appendChild(trEl);
     this.routes[`route${this.counterRoutes}`] = trEl;
-
     this.counterRoutes++;
   }
 
@@ -165,14 +203,19 @@ class Capsule {
     return this.capsuleHTMLEl;
   }
 
+  /**
+   * Удаления маршрута из DOM и this.routes
+   * @param {string} idRoute  
+   */
   removeRoute(idRoute) {
     let route = this.routes[idRoute];
     route.remove();
+    delete this.routes[idRoute];
+    console.log(this.routes);
   }
 
   /**
-  * Создаёт datalist для выбора для выбора пунктов назначения/отправления в инпутах.
-  * 
+  * Создаёт datalist для выбора пунктов назначения/отправления в инпутах. 
   * @returns {HTMLElement} datalist для выбора для выбора пунктов назначения/отправления в инпутах.
   */
   createDatalistCapsuleStartingPointAndDestinationHTMLEl() {
@@ -186,5 +229,22 @@ class Capsule {
     }
 
     return this.datalistCapsuleStartingPointAndDestinationHTMLEl;
+  };
+
+  /**
+  * Создаёт datalist для выбора периодичности в инпутах. 
+  * @returns {HTMLElement} datalist для выбора периодичности в инпутах.
+  */
+  createDatalistCapsuleRepeatVariantsHTMLEl() {
+    this.datalistCapsuleRepeatVariantsHTMLEl = document.createElement('datalist');
+    this.datalistCapsuleRepeatVariantsHTMLEl.id = 'datalistCapsuleRepeatVariantsHTMLEl';
+
+    for (let variant of this.repeatVariants) {
+      let optionEl = document.createElement('option');
+      optionEl.value = variant;
+      this.datalistCapsuleRepeatVariantsHTMLEl.appendChild(optionEl);
+    }
+
+    return this.datalistCapsuleRepeatVariantsHTMLEl;
   };
 }
