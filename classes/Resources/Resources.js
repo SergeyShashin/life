060cc3ed97a046966efdabaf77ead09b4a9dd412
SergeyShashin <s9997910@yandex.ru>
2025-12-25 10:29:50 +0300

    Life 251225 updated.

---
+++ classes/Resources/Resources.js
@@ -0,0 +1,313 @@
+'use strict';
+
+class Resources {
+  constructor() {
+  }
+  resources = {
+    people: {
+      availability: 8341290530,
+      free: 0,
+      needsСreated: 0,
+      nameRu: 'люди',
+      symbol: ':)',
+      unitsMeasurement: '',
+    },
+    landArea: {
+      availability: 149000000,
+      free: 0,
+      needsСreated: 0,
+      nameRu: 'земля',
+      symbol: 'landArea',
+      unitsMeasurement: 'km'
+    },
+    earth: {
+      availability: 149000000,
+      free: 0,
+      needsСreated: 0,
+      nameRu: 'земля',
+      symbol: 'earth',
+      unitsMeasurement: 't'
+    },
+    waterArea: {
+      availability: 361000000,
+      free: 0,
+      needsСreated: 0,
+      nameRu: 'вода',
+      symbol: 'waterArea',
+      unitsMeasurement: 'km'
+    },
+    water: {
+      availability: 361000000,
+      free: 0,
+      needsСreated: 0,
+      nameRu: 'вода',
+      symbol: 'water',
+      unitsMeasurement: 'l'
+    },
+    freshWater: {
+      availability: 35000000,
+      free: 0,
+      needsСreated: 0,
+      nameRu: 'пресная вода',
+      symbol: 'freshWater',
+      unitsMeasurement: 'l'
+    },
+    hydrogen: {
+      availability: 5000000000000,
+      free: 0,
+      needsСreated: 0,
+      nameRu: 'водород',
+      symbol: 'H',
+      unitsMeasurement: 't'
+    },
+    oxygen: {
+      availability: '?',
+      nameRu: 'кислород',
+    },
+    helium: {
+      availability: '?',
+      nameRu: 'гелий',
+    },
+    lithium: {
+      availability: '?',
+      nameRu: 'литий',
+    },
+    beryllium: {
+      availability: '?',
+      nameRu: 'бериллий',
+    },
+    boron: {
+      availability: '?',
+      nameRu: 'бор',
+    },
+    carbon: {
+      availability: '?',
+      nameRu: 'углерод',
+    },
+    nitrogen: {
+      availability: '?',
+      nameRu: 'азот',
+    },
+    fluorine: {
+      availability: '?',
+      nameRu: 'фтор',
+    },
+    neon: {
+      availability: '?',
+      nameRu: 'неон',
+    },
+    sodium: {
+      availability: '?',
+      nameRu: 'натрий',
+    },
+    magnesium: {
+      availability: '?',
+      nameRu: 'магний',
+    },
+    aluminum: {
+      availability: '?',
+      nameRu: 'алюминий',
+    },
+    silicon: {
+      availability: '?',
+      nameRu: 'кремний',
+    },
+    phosphorus: {
+      availability: '?',
+      nameRu: 'фосфор',
+    },
+    sulfur: {
+      availability: '?',
+      nameRu: 'сера',
+    },
+    chlorine: {
+      availability: '?',
+      nameRu: 'хлор',
+    },
+    argon: {
+      availability: '?',
+      nameRu: 'аргон',
+    },
+    potassium: {
+      availability: '?',
+      nameRu: 'калий',
+    },
+    calcium: {
+      availability: '?',
+      nameRu: 'кальций',
+    },
+    scandium: {
+      availability: '?',
+      nameRu: 'скандий',
+    },
+    titanium: {
+      availability: '?',
+      nameRu: 'титан',
+    },
+    vanadium: {
+      availability: '?',
+      nameRu: 'ванадий',
+    },
+    chromium: {
+      availability: '?',
+      nameRu: 'хром',
+    },
+    manganese: {
+      availability: '?',
+      nameRu: 'марганец',
+    },
+    iron: {
+      availability: '?',
+      nameRu: 'железо',
+    },
+    cobalt: {
+      availability: '?',
+      nameRu: 'железо',
+    },
+    nickel: {
+      availability: '?',
+      nameRu: 'никель',
+    },
+    copper: {
+      availability: '?',
+      nameRu: 'медь',
+    },
+    zinc: {
+      availability: '?',
+      nameRu: 'цинк',
+    },
+    gallium: {
+      availability: '?',
+      nameRu: 'галий',
+    },
+    germanium: {
+      availability: '?',
+      nameRu: 'германий',
+    },
+    garsenic: {
+      availability: '?',
+      nameRu: 'мышьяк',
+    },
+    selenium: {
+      availability: '?',
+      nameRu: 'селен',
+    },
+    bromine: {
+      availability: '?',
+      nameRu: 'бром',
+    },
+    rubidium: {
+      availability: '?',
+      nameRu: 'рубидий',
+    },
+    strontium: {
+      availability: '?',
+      nameRu: 'стронций',
+    },
+    zirconium: {
+      availability: '?',
+      nameRu: 'цирконий',
+    },
+    molybdenum: {
+      availability: '?',
+      nameRu: 'молибден',
+    },
+    rhodium: {
+      availability: '?',
+      nameRu: 'родий',
+    },
+    palladium: {
+      availability: '?',
+      nameRu: 'палладий',
+    },
+    cadmium: {
+      availability: '?',
+      nameRu: 'кадмий',
+    },
+    indium: {
+      availability: '?',
+      nameRu: 'индий',
+    },
+    tin: {
+      availability: '?',
+      nameRu: 'олово',
+    },
+    tellurium: {
+      availability: '?',
+      nameRu: 'теллур',
+    },
+    xenon: {
+      availability: '?',
+      nameRu: 'ксенон',
+    },
+    cesium: {
+      availability: '?',
+      nameRu: 'цезий',
+    },
+    barium: {
+      availability: '?',
+      nameRu: 'барий',
+    },
+    lanthanum: {
+      availability: '?',
+      nameRu: 'лантан',
+    },
+    cerium: {
+      availability: '?',
+      nameRu: 'церий',
+    },
+    neodymium: {
+      availability: '?',
+      nameRu: 'неодим',
+    },
+    terbium: {
+      availability: '?',
+      nameRu: 'тербий',
+    },
+    wolfram: {
+      availability: '?',
+      nameRu: 'вольфрам',
+    },
+    osmium: {
+      availability: '?',
+      nameRu: 'осмий',
+    },
+    platinum: {
+      availability: '?',
+      nameRu: 'платина',
+    },
+    gold: {
+      availability: '?',
+      nameRu: 'золото',
+    },
+    mercury: {
+      availability: '?',
+      nameRu: 'ртуть',
+    },
+    thallium: {
+      availability: '?',
+      nameRu: 'талий',
+    },
+    plumbum: {
+      availability: '?',
+      nameRu: 'свинец',
+    },
+    bismuth: {
+      availability: '?',
+      nameRu: 'висмут',
+    },
+    thorium: {
+      availability: '?',
+      nameRu: 'торий',
+    },
+    uranium: {
+      availability: '?',
+      nameRu: 'уран',
+    },
+    plutonium: {
+      availability: '?',
+      nameRu: 'плутоний',
+    },
+  };
+
+}
